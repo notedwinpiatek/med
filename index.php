ac092536d7ad98d2800ede0340c6af0c71a50d92
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2022-03-23 11:02:56 +0100

    die

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
-echo "Hello world";
+$db = new mysqli("localhost", "root", "", "med");
+
+$q = $db->prepare("SELECT * FROM staff");
+if($q->execute()){
+    $result = $q->get_result();
+    while($row = $result->fetch_assoc()){
+        $id = $row['id'];
+        $firstName = $row['firstName'];
+        $lastName = $row['lastName'];
+        echo "Lekarz $firstName $lastName<br>";
+    }
+} else {
+
+    die("Błąd pobierania z bazy danych");
+}
 
 ?>
