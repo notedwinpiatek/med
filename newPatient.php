ec748a83c0606871165d53cd7a0b678abff8b6fc
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2022-03-30 11:30:14 +0200

    newpatient

--- newPatient.php
+++ newPatient.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_REQUEST['firstName']) && isset($_REQUEST['lastName'])) {
     $db = new mysqli("localhost", "root", "", "med");
-    $q = $db->prepare("INSERT INTO patient VALUES (NULL, ?, ?, ?, ?");
+    $q = $db->prepare("INSERT INTO patient VALUES (NULL, ?, ?, ?, ?)");
     $q->bind_param("ssss", $_REQUEST['firstName'], $_REQUEST['lastName'], 
                             $_REQUEST['phone'], $_REQUEST['pesel']);
     if($q->execute()) {
@@ -9,10 +9,11 @@ if(isset($_REQUEST['firstName']) && isset($_REQUEST['lastName'])) {
     }
 } else {
     echo '
-    <form action="newPatient.php" method="post"></form>
+    <form action="newPatient.php" method="post">
     <label for="firstName">Imię</label>
     <input type="text" name="firstName" id="firstName">
-    <label for="lastName" name="lastName" id="lastName">Nazwisko</label>
+    <label for="lastName">Nazwisko</label>
+    <input type="text"  name="lastName" id="lastName">
     <label for="phone">Numer telefonu</label>
     <input type="text" name="phone" id="phone">
     <label for="pesel">Numer PESEL</label>
