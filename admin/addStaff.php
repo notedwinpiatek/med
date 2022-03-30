f7089113d5cc34fa275439a584e77ebb440b0e33
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2022-03-30 10:06:35 +0200

    addStaff

---
+++ admin/addStaff.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_REQUEST['firstName']) && isset($_REQUEST['lastName'])) {
+    $db = new mysqli("localhost", "root", "", "med");
+    $q = $db->prepare("INSERT INTO staff VALUES(NULL, ?, ?");
+    $q->bind_param("SS", $_REQUEST['firstName'], $_REQUEST['lastName']);
+    if($q->execute()) {
+        echo "Dodano nowy personel";
+    }
+    else {
+        echo '
+        <form action="addStaff.php" method="post"></form>
+        <label for="firstName">Imię</label>
+        <input type="text" name="firstName" id="firstName">
+        <label for="lastName">Nazwisko</label>
+        <input type="text" name="lastName" id="lastName">
+        <input type="submit" value="Dodaj nowy personel">
+        </form>';
+    }
+}
+
+
+?>
+
+
+
