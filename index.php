b5b400f309865828b446aa3d397d9516d8de164d
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2022-03-30 09:31:32 +0200

    test

--- index.php
+++ index.php
@@ -1,34 +1,33 @@
 <?php
 
 $db = new mysqli("localhost", "root", "", "med");
-
 $q = $db->prepare("SELECT * FROM staff");
-if ($q && $q->execute()) {
+if($q && $q->execute()) {
     $result = $q->get_result();
-    while ($staff = $result->fetch_assoc()) {
+    while($staff = $result->fetch_assoc()) {
         $staffId = $staff['id'];
         $firstName = $staff['firstName'];
         $lastName = $staff['lastName'];
         echo "Lekarz $firstName $lastName<br>";
         $q = $db->prepare("SELECT * FROM appointment WHERE staff_id = ?");
         $q->bind_param("i", $staffId);
-        if ($q && $q->execute()) {
+        if($q && $q->execute()) {
             $appointments = $q->get_result();
-            while ($appointment = $appointments->fetch_assoc()) {
-                $appointmentsId = $appointment['id'];
+            while($appointment = $appointments->fetch_assoc()) {
+                $appointmentId = $appointment['id'];
                 $appointmentDate = $appointment['date'];
-                $appointmentTimeStamp = strtotime($appointmentDate);
-                echo "<a href=\"appointment.phpstyle=\"margin:10px; display: \">";
-                echo date("j.m H:i", $appointmentTimeStamp);
+                $appointmentTimestamp = strtotime($appointmentDate);
+                echo "<a href=\"appointment.php?id=$appointmentId\" style=\"margin:10px; display:block\">";
+                echo date("j.m H:i", $appointmentTimestamp);
                 echo "</a>";
             }
             echo "<br>";
         } else {
-            die("Błąd pobierania z bazy danych");
+            die("Błąd pobierania wizyt z bazy danych");
         }
     }
 } else {
-
-    die("Błąd pobierania z bazy danych");
+    die("Błąd pobierania lekarzy z bazy danych");
 }
+
 ?>
