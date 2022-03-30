31ba9a705e1e4167fafcc46daf83f12229398e3e
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2022-03-30 10:25:49 +0200

    newPatient

---
+++ newPatient.php
@@ -0,0 +1,11 @@
+<?php
+
+
+
+?>
+
+<form action="newPatient.php" method="post"></form>
+    <label for="firstName">Imię</label>
+    <input type="text" name="firstName" id="firstName">
+    
+</form>
