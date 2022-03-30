ca121e1346b40dd00c6f2901e1501e4f87d56d7d
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2022-03-30 10:31:26 +0200

    newPatient

--- newPatient.php
+++ newPatient.php
@@ -1,11 +1,20 @@
 <?php
+if(isset($_REQUEST['firstName']) && isset($_REQUEST['lastName'])) {
+
+} else {
+    echo '
+    <form action="newPatient.php" method="post"></form>
+    <label for="firstName">Imię</label>
+    <input type="text" name="firstName" id="firstName">
+    <label for="lastName" name="lastName" id="lastName">Nazwisko</label>
+    <label for="pesel">Numer PESEL</label>
+    <input type="text" name="pesel" id="pesel">
+    <input type="submit" value="Zapisz">
+    </form>
+    ';
+}
 
 
 
 ?>
-
-<form action="newPatient.php" method="post"></form>
-    <label for="firstName">Imię</label>
-    <input type="text" name="firstName" id="firstName">
-    
-</form>
+<input type="text" name="" id="">
